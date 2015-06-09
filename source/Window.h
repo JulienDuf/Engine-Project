d254fd3da9fe5785139e9fb266b743cbaca3daec
Julien Dufresne <dufresne_julien@hotmail.ca>
2015-06-08 21:50:27 -0400

    Welcome to SDL2 and AVL tree

--- source/Window.h
+++ source/Window.h
@@ -1,30 +1,46 @@
 #pragma once
+#include <iostream>
 
 class Window {
 
 private:
-	GLFWwindow* window;
+	SDL_Window* window;
+	SDL_GLContext windowContext;
+	SDL_Event event;
+
+	bool activeWindow;
 public:
 
-	Window(int w, int h, const char* nom) {
+	Window(int w, int h, const char* name) {
 
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
-		window = glfwCreateWindow(w, h, nom, NULL, NULL);
-		glfwMakeContextCurrent(window);
-
+		window = SDL_CreateWindow(name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_OPENGL);
+		windowContext = SDL_GL_CreateContext(window);
+		SDL_GL_MakeCurrent(window, windowContext);
+		activeWindow = true;
 	}
 
 	// true open
 	// false close
 	bool windowState() {
-		return glfwWindowShouldClose(window);
+		return activeWindow;
 	}
 
 	void renderWindow() {
-		glfwSwapBuffers(window);
+		SDL_GL_SwapWindow(window);
+	}
+
+	void pollEvent() {
+
+		while (SDL_PollEvent(&event)) {
+
+			switch (event.type) {
+			case SDL_QUIT:
+				activeWindow = false;
+				break;
+			}
+		}
 	}
 };
